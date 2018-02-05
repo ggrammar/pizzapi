4be08347ea9174ecfb6ea12ade18e1e7bf589306
gamagori <grant.mattke.gordon@gmail.com>
2018-02-04 22:36:20 -0600

    initial commit of address docstrings for sphinx testing

--- pizzapi/address.py
+++ pizzapi/address.py
@@ -3,6 +3,14 @@ from .utils import request_json
 from .urls import Urls, COUNTRY_USA
 
 class Address(object):
+    """
+    Create an address, for finding stores and placing orders.
+
+    The Address object describes a street address in North America (USA or
+    Canada, for now). Callers can use the Address object's methods to find
+    the closest or nearby stores from the API. 
+    """
+
     def __init__(self, street, city, region='', zip='', country=COUNTRY_USA, *args):
         self.street = street.strip()
         self.city = city.strip()
@@ -25,6 +33,13 @@ class Address(object):
         return '{City}, {Region}, {PostalCode}'.format(**self.data)
 
     def nearby_stores(self, service='Delivery'):
+        """
+        Query the API to find nearby stores.
+
+        nearby_stores will filter the information we receive from the API
+        to exclude stores that are not currently online (!['IsOnlineNow']),
+        and stores that are not currently in service (!['ServiceIsOpen']).
+        """
         data = request_json(self.urls.find_url(), line1=self.line1, line2=self.line2, type=service)
         return [Store(x, self.country) for x in data['Stores']
                 if x['IsOnlineNow'] and x['ServiceIsOpen'][service]]
