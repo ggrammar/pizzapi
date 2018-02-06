c5ce57422e608926115f9e18c2ed849cc74cd69b
gamagori <grant.mattke.gordon@gmail.com>
2018-02-05 20:56:25 -0600

    added documentation, split up some lengthy method calls

--- pizzapi/track.py
+++ pizzapi/track.py
@@ -3,11 +3,27 @@ from .utils import request_xml, request_json
 
 
 def track_by_phone(phone, country=COUNTRY_USA):
+    """Query the API to get tracking information.
+
+    Not quite sure what this gets you - problem to solve for next time I get pizza. 
+    """
     phone = str(phone).strip()
-    data = request_xml(Urls(country).track_by_phone(), phone=phone)['soap:Envelope']['soap:Body']
+    data = request_xml(
+        Urls(country).track_by_phone(), 
+        phone=phone
+    )['soap:Envelope']['soap:Body']
+
     response = data['GetTrackerDataResponse']['OrderStatuses']['OrderStatus']
+
     return response
 
 
 def track_by_order(store_id, order_key, country=COUNTRY_USA):
-    return request_json(Urls(country).track_by_order(), store_id=store_id, order_key=order_key)
+    """Query the API to get tracking information.
+    """
+    return request_json(
+        Urls(country).track_by_order(),
+        store_id=store_id,
+        order_key=order_key
+    )
+
