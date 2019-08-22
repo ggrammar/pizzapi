2a67ceb0f6df285988f1a2d7b678bbd2526c26a4
Grant <grant.mattke.gordon@gmail.com>
2019-08-21 22:16:33 -0400

    version bump, just to make sure we have it all

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.5',
+    version='0.0.6',
 
     description='A Python wrapper for the Dominos Pizza API',
     long_description=long_description,
@@ -100,8 +100,6 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         # 'Programming Language :: Python :: 3',
         # 'Programming Language :: Python :: 3.3',
         # 'Programming Language :: Python :: 3.4',
